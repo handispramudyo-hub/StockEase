8c127e602448608cc9d1c766b26858f750d33a91
111202314883 <111202314883@mhs.dinus.ac.id>
2026-07-29 15:19:25 +0700

    hapus script reset password

--- reset_pass.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=hank7435_tamaDB', 'hank7435_tamaDB', 'tamaganteng');
-$hash = password_hash('password', PASSWORD_BCRYPT);
-$db->exec("UPDATE users SET password=" . $db->quote($hash) . " WHERE email=" . $db->quote('admin@tama.com'));
-echo "Password untuk admin@tama.com direset ke: password\n";
